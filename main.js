bdb0a5d03c9d6f2fa305a3ee79a2ae7db4433b21
AAsipowich <AAsipowich@gmail.com>
2021-02-23 14:12:08 +0300

    Date format conversion. Search for accommodation facilities.

--- main.js
+++ main.js
@@ -91,24 +91,92 @@
 // }
 
 // Zero();
-// Покрасить абзацы по клику
-const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
-const paragText1 = document.getElementById('text1');
-const paragText2 = document.getElementById('text2');
-const paragText3 = document.getElementById('text3');
 
-function colorChange(parameter) {
-  let clicks = 0;
-  return function () {
-    clicks += 1;
-    const paragraph = parameter;
-    paragraph.style.color = colors[clicks - 1];
-    if (clicks > colors.length - 1) {
-      clicks = 0;
-    }
-  };
+// Покрасить абзацы по клику
+//const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+//const paragText1 = document.getElementById('text1');
+//const paragText2 = document.getElementById('text2');
+//const paragText3 = document.getElementById('text3');
+
+//function colorChange(parameter) {
+//let clicks = 0;
+//return function () {
+//clicks += 1;
+//const paragraph = parameter;
+//paragraph.style.color = colors[clicks - 1];
+//if (clicks > colors.length - 1) {
+// clicks = 0;
+//}
+//};
+//}
+
+//paragText1.addEventListener('click', colorChange(paragText1));
+//paragText2.addEventListener('click', colorChange(paragText2));
+//paragText3.addEventListener('click', colorChange(paragText3));
+//Преобразование формата даты
+function dateReplace(date) {
+  const dateReplaced = date.slice(8) + date.slice(4, 8) + date.slice(0, 4);
+  const dateWithDots = dateReplaced.replace(/-/g, '.');
+  console.log(dateWithDots);
 }
 
-paragText1.addEventListener('click', colorChange(paragText1));
-paragText2.addEventListener('click', colorChange(paragText2));
-paragText3.addEventListener('click', colorChange(paragText3));
+dateReplace('2020-11-26');
+
+const data = [
+  {
+    country: 'Russia',
+    city: 'Saint Petersburg',
+    hotel: 'Hotel Leopold',
+  },
+  {
+    country: 'Spain',
+    city: 'Santa Cruz de Tenerife',
+    hotel: 'Apartment Sunshine',
+  },
+  {
+    country: 'Slowakia',
+    city: 'Vysokie Tatry',
+    hotel: 'Villa Kunerad',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hostel Friendship',
+  },
+  {
+    country: 'Indonesia',
+    city: 'Bali',
+    hotel: 'Ubud Bali Resort&SPA',
+  },
+  {
+    country: 'Netherlands',
+    city: 'Rotterdam',
+    hotel: 'King Kong Hostel',
+  },
+  {
+    country: 'Marocco',
+    city: 'Ourika',
+    hotel: 'Rokoko Hotel',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hotel Rehberge Berlin Mitte',
+  },
+];
+
+function searchData(searchWord) {
+  let foundString;
+  for (let i = 0; i < data.length; i += 1) {
+    if (data[i].country.includes(searchWord) || data[i].city.includes(searchWord)
+     || data[i].hotel.includes(searchWord)) {
+      foundString = `${data[i].country}, ${data[i].city}, ${data[i].hotel}`;
+      console.log(foundString);
+    }
+  }
+}
+
+searchData('Germany');
+
+module.exports = dateReplace;
+module.exports = searchData;
