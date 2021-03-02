084b2d6b3bd4c1c00c440999a7271a069b0e2a45
AAsipowich <AAsipowich@gmail.com>
2021-03-02 12:00:03 +0300

    1. palindrome function 2. Search for accommodation facilities. 3. We compared the countries with the cities from the array

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+/* eslint-disable no-console */
 const user = 'John Doe';
+// eslint-disable-next-line no-console
 console.log(user);
 
 let student = 'Anya';
+// eslint-disable-next-line no-console
 console.log(student);
 // В переменной user переменная student
 student = user;
+// eslint-disable-next-line no-console
 console.log(user);
 
 let test = 1;
@@ -13,31 +17,37 @@ test = ++test;
 
 test += '1';
 // В переменной test значение 21
+// eslint-disable-next-line no-console
 console.log(test);
 
 test = --test;
 
 // В переменной test значение 20
+// eslint-disable-next-line no-console
 console.log(test);
 
 test = !test;
 // В переменной test значение false
+// eslint-disable-next-line no-console
 console.log(test);
 
 let bol = 1;
 const bal = [2, 3, 5, 8];
 for (let i = 0; i <= bal.length; i++) {
   bol *= bal[i];
+  // eslint-disable-next-line no-console
   console.log(bol);
 }
 
 const bac = [2, 5, 8, 15, 0, 6, 20, 3];
 for (let i = 0; i <= bac.length; i++) {
+  // eslint-disable-next-line no-console
   if (bac[i] > 5 && bac[i] < 10) console.log(bac[i]);
 }
 
 const tro = [2, 5, 8, 15, 0, 6, 20, 3];
 for (let i = 0; i <= tro.length; i++) {
+  // eslint-disable-next-line eqeqeq
   if (tro[i] % 2 == 0) console.log(tro[i]);
 }
 
@@ -46,6 +56,7 @@ for (let i = 0; i <= tro.length; i++) {
 function palindrome(str) {
   return str === str.split('').reverse('').join('');
 }
+// eslint-disable-next-line no-console
 console.log(palindrome('шалаш'));
 
 // Функция min(a, b) и функция max(a,b)
@@ -55,9 +66,11 @@ function min(a, b) {
   }
   return b;
 }
+// eslint-disable-next-line no-console
 console.log(min(9, 5));
 
 function max(a, b) {
+  // eslint-disable-next-line no-debugger
   debugger;
   if (a > b) {
     return a;
@@ -98,6 +111,7 @@ const paragText3 = document.getElementById('text3');
 
 function colorChange(parameter) {
   let clicks = 0;
+  // eslint-disable-next-line func-names
   return function () {
     clicks += 1;
     const paragraph = parameter;
@@ -176,3 +190,192 @@ function searchString(searchWord) {
 }
 
 searchString('Germany');
+
+const wordPalindrome = 'шалаш';
+const reverseWordPalindrome = wordPalindrome.split().reverse().join();
+console.log(wordPalindrome === reverseWordPalindrome);
+
+const hotels = [
+  {
+    name: 'Hotel Leopold',
+    city: 'Saint Petersburg',
+    country: 'Russia',
+  },
+  {
+    name: 'Apartment Sunshine',
+    city: 'Santa Cruz de Tenerife',
+    country: 'Spain',
+  },
+  {
+    name: 'Villa Kunerad',
+    city: 'Vysokie Tatry',
+    country: 'Slowakia',
+  },
+  {
+    name: 'Hostel Friendship',
+    city: 'Berlin',
+    country: 'Germany',
+  },
+  {
+    name: 'Radisson Blu Hotel',
+    city: 'Kyiv',
+    country: 'Ukraine',
+  },
+  {
+    name: 'Paradise Hotel',
+    city: 'Guadalupe',
+    country: 'Mexico',
+  },
+  {
+    name: 'Hotel Grindewald',
+    city: 'Interlaken',
+    country: 'Switzerland',
+  },
+  {
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+  },
+  {
+    name: 'Virgin Hotel',
+    city: 'Chicago',
+    country: 'USA',
+  },
+  {
+    name: 'Grand Beach Resort',
+    city: 'Dubai',
+    country: 'United Arab Emirates',
+  },
+  {
+    name: 'Shilla Stay',
+    city: 'Seoul',
+    country: 'South Korea',
+  },
+  {
+    name: 'San Firenze Suites',
+    city: 'Florence',
+    country: 'Italy',
+  },
+  {
+    name: 'The Andaman Resort',
+    city: 'Port Dickson',
+    country: 'Malaysia',
+  },
+  {
+    name: 'Black Penny Villas',
+    city: 'BTDC, Nuca Dua',
+    country: 'Indonesia',
+  },
+  {
+    name: 'Koko Hotel',
+    city: 'Tokyo',
+    country: 'Japan',
+  },
+  {
+    name: 'Ramada Plaza',
+    city: 'Istanbul',
+    country: 'Turkey',
+  },
+  {
+    name: 'Waikiki Resort Hotel',
+    city: 'Hawaii',
+    country: 'USA',
+  },
+  {
+    name: 'Puro Hotel',
+    city: 'Krakow',
+    country: 'Poland',
+  },
+  {
+    name: 'Asma Suites',
+    city: 'Santorini',
+    country: 'Greece',
+  },
+  {
+    name: 'Cityden Apartments',
+    city: 'Amsterdam',
+    country: 'Netherlands',
+  },
+  {
+    name: 'Mandarin Oriental',
+    city: 'Miami',
+    country: 'USA',
+  },
+  {
+    name: 'Concept Terrace Hotel',
+    city: 'Rome',
+    country: 'Italy',
+  },
+  {
+    name: 'Ponta Mar Hotel',
+    city: 'Fortaleza',
+    country: 'Brazil',
+  },
+  {
+    name: 'Four Seasons Hotel',
+    city: 'Sydney',
+    country: 'Australia',
+  },
+  {
+    name: 'Le Meridien',
+    city: 'Nice',
+    country: 'France',
+  },
+  {
+    name: 'Apart Neptun',
+    city: 'Gdansk',
+    country: 'Poland',
+  },
+  {
+    name: 'Lux Isla',
+    city: 'Ibiza',
+    country: 'Spain',
+  },
+  {
+    name: 'Nox Hostel',
+    city: 'London',
+    country: 'UK',
+  },
+  {
+    name: 'Leonardo Vienna',
+    city: 'Vienna',
+    country: 'Austria',
+  },
+  {
+    name: 'Adagio Aparthotel',
+    city: 'Edinburgh',
+    country: 'UK',
+  },
+  {
+    name: 'Steigenberger Hotel',
+    city: 'Hamburg',
+    country: 'Germany',
+  },
+];
+function searchHotels(searchWord) {
+  const searchWordToLowerCase = searchWord.toLowerCase();
+  hotels.forEach((element) => {
+    const searchLineToLowerCase = element.name.toLowerCase() + element.city.toLowerCase()
+      + element.country.toLowerCase();
+    if (searchLineToLowerCase.includes(searchWordToLowerCase)) {
+      console.log(`${element.country}, ${element.city}, ${element.name}`);
+    }
+  });
+}
+
+searchHotels('south');
+
+function searchCountry() {
+  return hotels.reduce((accumulator, currentValue) => {
+    const resultSearch = [];
+    if (!accumulator[currentValue.country]) {
+      resultSearch.push(currentValue.city);
+      accumulator[currentValue.country] = resultSearch;
+    } else if (!accumulator[currentValue.country].includes(currentValue.city)) {
+      accumulator[currentValue.country].push(currentValue.city);
+    }
+    return accumulator;
+  }, {});
+}
+
+console.log(searchCountry());
--- main.test.js
+++ main.test.js
@@ -1,12 +1,17 @@
 function sum(a) {
- debugger
-  return function(b) {
+  // eslint-disable-next-line no-debugger
+  debugger;
+  // eslint-disable-next-line func-names
+  return function (b) {
     return a + b;
   };
 }
 
+// eslint-disable-next-line no-console
 console.log(sum(5)(2));
 
+// eslint-disable-next-line no-undef
 test('adds 5 + 2 to equal 7', () => {
+  // eslint-disable-next-line no-undef
   expect(sum(5)(2)).toBe(7);
 });
